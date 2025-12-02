c86aaebbedfd1862aa65c197bf4a12161fd330a1
Vaibhav Dongre <21vaibhavdongre@gmail.com>
2025-12-02 19:43:50 +0530

    Backend url updated

--- frontend/secureelect/src/services/api.js
+++ frontend/secureelect/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://secureelect-deploy.onrender.com",
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 //add JWT token if exists
