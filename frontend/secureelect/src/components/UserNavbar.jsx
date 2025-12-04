b99d09616d6b687143660fc0856605875754d90a
Vaibhav Dongre <97147485+VaibhavDongre@users.noreply.github.com>
2025-12-04 16:35:29 +0530

    UserNavbar updated - extra button removed

--- frontend/secureelect/src/components/UserNavbar.jsx
+++ frontend/secureelect/src/components/UserNavbar.jsx
@@ -11,21 +11,21 @@ export default function UserNavbar() {
     return (
         <nav className="navbar navbar-dark bg-primary px-4 d-flex justify-content-between align-items-center">
 
-            {/* LEFT SIDE */}
+
             <Link to="/user/home" className="navbar-brand fw-bold">
                 SecureElect User
             </Link>
 
-            {/* RIGHT SIDE BUTTONS */}
+
             <div className="d-flex align-items-center">
 
-                <Link to="/user/home" className="btn btn-outline-light me-3">
+                {/* <Link to="/user/home" className="btn btn-outline-light me-3">
                     Home
                 </Link>
 
                 <Link to="/user/vote" className="btn btn-outline-light me-3">
                     Vote
-                </Link>
+                </Link> */}
 
                 <button className="btn btn-light" onClick={logout}>
                     Logout
